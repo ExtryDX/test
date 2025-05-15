7342338cb784c1641c422673cbbdfaf818d00b2a
ExtryDX <xavery.czuraj@gmail.com>
2025-05-15 23:47:12 +0200

    initial

---
+++ APBD8/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using APBD8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD8.Controllers;
+
+[ApiController]
+[Route("students")]
+public class StudentsController : ControllerBase
+{
+    private List<Student> _students = new List<Student>();
+
+    public StudentsController()
+    {
+        _students.Add(new Student { FirstName = "John", LastName = "Doe" });
+        _students.Add(new Student { FirstName = "Jane", LastName = "Doe" });
+        _students.Add(new Student { FirstName = "Julie", LastName = "Doe" });
+    }
+    
+    
+    [HttpGet]
+    public IActionResult GetStudents(string orderBy)
+    {
+        if (_students == null)
+        {
+            return NotFound();
+        }
+        return Ok(_students);
+    }
+    
+    [HttpGet("{firstName}")]
+    public IActionResult GetStudent(string firstName)
+    {
+        var student = _students.FirstOrDefault(s => s.FirstName == firstName);
+        if (student == null)
+        {
+            return NotFound();
+        }
+        return Ok(student);
+    }
+    
+
+}
---
+++ APBD8/Models/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD8.Models;
+
+public class Student
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+}
